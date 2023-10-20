3b0623b23336270439c7bc46a420b15256bdccf5
nesterch <terlemez.neslihan@gmail.com>
2023-10-20 13:45:17 +0200

    US_03 ve US_04 featurefiles ve stepdefinition olusturuldu

--- src/test/java/StepDefinitons/_03_BelgeTurleriYonetimiDocumentTypesSteps.java
+++ src/test/java/StepDefinitons/_03_BelgeTurleriYonetimiDocumentTypesSteps.java
@@ -1,4 +1,32 @@
+
 package StepDefinitons;
 
+import io.cucumber.java.en.*;
+
 public class _03_BelgeTurleriYonetimiDocumentTypesSteps {
+
+    @Given("Navigate to Document Types")
+    public void navigate_to_document_types() {
+
+    }
+    @When("Create a new document")
+    public void create_a_new_document() {
+
+    }
+    @Then("document should be added")
+    public void document_should_be_added() {
+
+    }
+    @Then("user edit document")
+    public void user_edit_document() {
+
+    }
+    @Then("delete document")
+    public void delete_document() {
+
+    }
+    @Then("document should be deleted")
+    public void document_should_be_deleted() {
+
+    }
 }
--- src/test/java/StepDefinitons/_04_AdminPanelineYeniAlanlarEklemeSteps.java
+++ src/test/java/StepDefinitons/_04_AdminPanelineYeniAlanlarEklemeSteps.java
@@ -1,27 +1,28 @@
+
 package StepDefinitons;
-import Pages.POM_04;
-import Utilities.WBA;
+
 import io.cucumber.java.en.*;
 
 public class _04_AdminPanelineYeniAlanlarEklemeSteps {
 
-    POM_04 lc= new POM_04();
-    @Given("Navigate to Set Up Categories")
-    public void navigate_to_set_up_categories() {
-    WBA.getDriver().get("https://test.mersys.io/");
-    WBA.getDriver().manage().window().maximize();
-    }
-    @When("Create new Fields")
-    public void create_new_fields() {
+@Given("Navigate to Fields Page")
+public void navigate_to_fields_page() {
 
-    }
-    @When("add a Field")
-    public void add_a_field() {lc.myClick(lc.Setup); }
-    @When("edit a Field")
-    public void edit_a_field() {lc.myClick(lc.addBtn); }
-    @When("delete a Field")
-    public void delete_a_field() {lc.myClick(lc.addBtn); }
-    @When("User should delete successfully")
-    public void user_should_delete_successfully() {
-        lc.verifyContainsText(lc.Fields, "successfully");}
-}
+        }
+@When("Create a new Field")
+public void create_a_new_field() {
+
+        }
+@When("edit a Field")
+public void edit_a_field() {
+
+        }
+@When("delete a Field")
+public void delete_a_field() {
+
+        }
+@When("User should delete successfully")
+public void user_should_delete_successfully() {
+
+        }
+

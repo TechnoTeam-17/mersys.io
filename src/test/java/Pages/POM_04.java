7963ab32e3ced305c70ba0b986b0c4355e11e2c5
nesterch <terlemez.neslihan@gmail.com>
2023-10-23 20:11:51 +0200

    US_03 uzerinde calisiyorum

--- src/test/java/Pages/POM_04.java
+++ src/test/java/Pages/POM_04.java
@@ -4,6 +4,7 @@ import Utilities.Events;
 import Utilities.WBA;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 public class POM_04 extends Events {
@@ -16,7 +17,7 @@ public class POM_04 extends Events {
     public WebElement Setup;
     @FindBy(xpath = "//*[text()='Parameters']")
     public WebElement Parameters;
-    @FindBy(xpath="//span[@class='nav-link-title ng-star-inserted']']")
+    @FindBy(xpath="(//span[text()='Fields'])[1]")
     public WebElement Fields;
 
     @FindBy(css = "ms-add-button")
@@ -25,18 +26,48 @@ public class POM_04 extends Events {
     @FindBy(css = "ms-dialog-content input[data-placeholder='Name']")
     public WebElement Name;
 
-    @FindBy(css = "ms-dialog-content input[data-placeholder='Name']")
-    public WebElement NumberBtn;
+    @FindBy(xpath = "(//input[@id='ms-text-field-11'])")
+    public WebElement Code;
+
+    @FindBy(xpath ="( //div[@class='mat-select-arrow ng-tns-c3082329526-135'])")
+    public WebElement FieldTypeBtn;
+
+    @FindBy(xpath="(//span[@class='mat-option-text'])[6]")
+    public WebElement LogicalBtn;
+
 
     @FindBy(xpath="//ms-save-button/button")
     public WebElement SaveBtn;
 
-    @FindBy(css = "ms-delete-button")
-    public WebElement deletBtn;
+    @FindBy(css = "ms-edit-button[class='ng-star-inserted']")
+    public WebElement EditBtn;
 
-    @FindBy(xpath="//div[contains(text(),'successfully')]")
+    @FindBy(xpath="//div[contains(text(),'Document successfully deleted')]")
     public WebElement successMessage;
 
+    @FindBy(xpath = "//mat-form-field//input[@data-placeholder='Name']")
+    public WebElement searchInput;
+
+    @FindBy(xpath = "//ms-search-button//button")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "(//ms-delete-button//button)[1]")
+    public WebElement deleteImageBtn;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement deleteDialogBtn;
+
+
+    public void deleteItem(String searchText) {
+        mySendKeys(searchInput, searchText);
+        myClick(searchButton);
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//fuse-progress-bar/*"), 0));
+        myClick(deleteImageBtn);
+        myClick(deleteDialogBtn);
+        // bu kodlar bir sitede belirlenen birseyi silmeyi sagliyor. once search yapiyor
+        // sonra bulduktan sonra delete yapiyor.
+
+    }
 }
 
 
--- src/test/java/StepDefinitons/_04_AddingNewFieldsToAdminPanel.java
+++ src/test/java/StepDefinitons/_04_AddingNewFieldsToAdminPanel.java
@@ -1,49 +1,52 @@
+
 package StepDefinitons;
+
 import Pages.POM_04;
 import Utilities.WBA;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class _04_AddingNewFieldsToAdminPanel {
+public class _04_AddingNewFieldsToAdminPanel extends POM_04{
 
     POM_04 lc= new POM_04();
-    Actions actions = new Actions(WBA.getDriver());
+
     @Given("Navigate to Fields Page")
     public void navigate_to_fields_page() {
         lc.myClick(lc.Setup);
         lc.myClick(lc.Parameters);
         lc.myClick(lc.Fields);
-        WBA.getDriver().manage().window().maximize();
-    }
+          }
 
     @When("Create a new Field")
     public void create_a_new_field() {
         lc.myClick(lc.addBtn);
-        lc.mySendKeys(lc.Name, "Middle Europa");
-        Action aksiyon = actions.moveToElement(lc.NumberBtn).build();
-        aksiyon.perform();
-        lc.myClick(lc.NumberBtn);
+        lc.mySendKeys(lc.Name, "Nord Europa");
+        lc.mySendKeys(lc.Code, "12345");
+        lc.myClick(lc.FieldTypeBtn);
+        lc.myClick(lc.LogicalBtn);
+        new Actions(WBA.getDriver()).sendKeys(Keys.ESCAPE).perform();
         lc.myClick(lc.SaveBtn);
     }
 
-    @When("edit a Field")
+    @And("edit a Field")
     public void edit_a_field() {
-
+        lc.myClick(lc.EditBtn);
+        lc.mySendKeys(lc.Name, "East Europa");
+        lc.myClick(lc.SaveBtn);
     }
 
-    @When("delete a Field")
+    @Then("delete a Field")
     public void delete_a_field() {
-       lc.myClick(lc.deletBtn);
+    lc.deleteItem("East Europa");
 
     }
 
-    @When("User should delete successfully")
+    @Then("User should delete successfully")
     public void user_should_delete_successfully() throws InterruptedException{
-         Thread.sleep(2000);
-          lc.verifyContainsText(lc.successMessage,"deleted");
+         lc.verifyContainsText(lc.successMessage,"deleted");
     }
-
 
     }
 

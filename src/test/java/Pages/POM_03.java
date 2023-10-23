91998857c4682662f65b88ce27e02bb9cb4e188a
nesterch <terlemez.neslihan@gmail.com>
2023-10-23 13:44:30 +0200

    03 de scroll ayarlamasi yapmaya calisiyorum

--- src/test/java/Pages/POM_03.java
+++ src/test/java/Pages/POM_03.java
@@ -15,7 +15,7 @@ public class POM_03 extends Events {
     public WebElement Setup;
     @FindBy (xpath = "//*[text()='Parameters']")
     public WebElement Parameters;
-    @FindBy(xpath="//span[@class='nav-link-title ng-star-inserted']']")
+    @FindBy(xpath = "(//span[text()='Document Types'])[1]")
     public WebElement DocumentTypes;
 
     @FindBy(css = "ms-add-button")
@@ -24,14 +24,17 @@ public class POM_03 extends Events {
     @FindBy(css = "ms-dialog-content input[data-placeholder='Name']")
     public WebElement Name;
 
-    @FindBy(xpath="//span[@class='mat-select-placeholder mat-select-min-line ng-tns-c3082329526-61 ng-star-inserted']']")
-    public WebElement StageBtn;
+   @FindBy(xpath="//div[@class='mat-select-arrow-wrapper ng-tns-c3082329526-133')]")
+   public WebElement StageBtn;
 
-    @FindBy(xpath="//div[contains(text(),'Registration')]")
+    @FindBy(xpath="//*[@class='mat-pseudo-checkbox mat-option-pseudo-checkbox mat-pseudo-checkbox-full ng-star-inserted mat-pseudo-checkbox-checked')]")
     public WebElement StudentRegistrationBtn;
 
     @FindBy(xpath="//ms-save-button/button")
     public WebElement SaveBtn;
+
+    @FindBy(css = "ms-edit-button[class='ng-star-inserted']")
+    public WebElement EditBtn;
 
     @FindBy(css = "ms-delete-button")
     public WebElement deletBtn;

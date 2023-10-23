5654b99300dc782398a7cf8ba1be208609122662
nesterch <terlemez.neslihan@gmail.com>
2023-10-23 15:41:45 +0200

    US_03 Bitti.

--- src/test/java/StepDefinitons/_03_DocumentTypesManegment.java
+++ src/test/java/StepDefinitons/_03_DocumentTypesManegment.java
@@ -5,20 +5,19 @@ import Pages.POM_03;
 import Utilities.Events;
 import Utilities.WBA;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class _03_DocumentTypesManegment extends Events {
+public class _03_DocumentTypesManegment extends POM_03 {
 
     POM_03 lc = new POM_03();
-
 
     @Given("Navigate to Document Types")
     public void navigate_to_document_types() {
         lc.myClick(lc.Setup);
         lc.myClick(lc.Parameters);
         lc.myClick(lc.DocumentTypes);
-
     }
 
     @When("Create a new document")
@@ -26,31 +25,24 @@ public class _03_DocumentTypesManegment extends Events {
         lc.myClick(lc.addBtn);
         lc.mySendKeys(lc.Name, "Education");
         lc.myClick(lc.StageBtn);
-        lc.scrollToElement(lc.StudentRegistrationBtn);
         lc.myClick(lc.StudentRegistrationBtn);
+        new Actions(WBA.getDriver()).sendKeys(Keys.ESCAPE).perform();
         lc.myClick(lc.SaveBtn);
     }
 
     @And("edit a document")
     public void user_edit_document() {
         lc.myClick(lc.EditBtn);
-        //element.clear();
         lc.mySendKeys(lc.Name, "Neslihan");
         lc.myClick(lc.SaveBtn);
-
     }
-
     @Then("delete document")
     public void delete_document() {
-        lc.myClick(lc.deletBtn);
+    lc.deleteItem("Neslihan");
 
     }
-
-    @Then("document should be deleted")
+  @Then("document should be deleted")
     public void document_should_be_deleted() throws InterruptedException {
-        Thread.sleep(2000);
-        lc.verifyContainsText(lc.successMessage, "deleted");
-
+     lc.verifyContainsText(lc.successMessage,"Document");
     }
-
 }

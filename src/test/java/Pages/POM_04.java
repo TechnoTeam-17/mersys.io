edd0d079aeb3bad401e517017cd77346fda3bab9
nesterch <terlemez.neslihan@gmail.com>
2023-10-24 09:53:19 +0200

    US_ 04 bitti

--- src/test/java/Pages/POM_04.java
+++ src/test/java/Pages/POM_04.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class POM_04 extends Events {
 
-    public POM_04 () {
+    public POM_04() {
         PageFactory.initElements(WBA.getDriver(), this);
     }
 
@@ -17,46 +17,28 @@ public class POM_04 extends Events {
     public WebElement Setup;
     @FindBy(xpath = "//*[text()='Parameters']")
     public WebElement Parameters;
-    @FindBy(xpath="(//span[text()='Fields'])[1]")
+    @FindBy(xpath = "(//span[text()='Fields'])[1]")
     public WebElement Fields;
-
     @FindBy(css = "ms-add-button")
     public WebElement addBtn;
-
     @FindBy(css = "ms-dialog-content input[data-placeholder='Name']")
     public WebElement Name;
-
-    @FindBy(xpath = "(//input[@id='ms-text-field-11'])")
+    @FindBy(css = "ms-dialog-content input[data-placeholder='Code']")
     public WebElement Code;
-
-    @FindBy(xpath ="( //div[@class='mat-select-arrow ng-tns-c3082329526-135'])")
-    public WebElement FieldTypeBtn;
-
-    @FindBy(xpath="(//span[@class='mat-option-text'])[6]")
-    public WebElement LogicalBtn;
-
-
-    @FindBy(xpath="//ms-save-button/button")
+    @FindBy(xpath = "//ms-save-button/button")
     public WebElement SaveBtn;
-
     @FindBy(css = "ms-edit-button[class='ng-star-inserted']")
     public WebElement EditBtn;
-
-    @FindBy(xpath="//div[contains(text(),'Document successfully deleted')]")
+    @FindBy(xpath = "//div[contains(text(),'Field successfully deleted')]")
     public WebElement successMessage;
-
     @FindBy(xpath = "//mat-form-field//input[@data-placeholder='Name']")
     public WebElement searchInput;
-
     @FindBy(xpath = "//ms-search-button//button")
     public WebElement searchButton;
-
     @FindBy(xpath = "(//ms-delete-button//button)[1]")
     public WebElement deleteImageBtn;
-
-    @FindBy(xpath = "//button[@type='submit']")
+    @FindBy(xpath = "//button [@type='submit']")
     public WebElement deleteDialogBtn;
-
 
     public void deleteItem(String searchText) {
         mySendKeys(searchInput, searchText);
--- src/test/java/StepDefinitons/_04_AddingNewFieldsToAdminPanel.java
+++ src/test/java/StepDefinitons/_04_AddingNewFieldsToAdminPanel.java
@@ -2,31 +2,24 @@
 package StepDefinitons;
 
 import Pages.POM_04;
-import Utilities.WBA;
 import io.cucumber.java.en.*;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 
-public class _04_AddingNewFieldsToAdminPanel extends POM_04{
+public class _04_AddingNewFieldsToAdminPanel extends POM_04 {
 
-    POM_04 lc= new POM_04();
+    POM_04 lc = new POM_04();
 
     @Given("Navigate to Fields Page")
     public void navigate_to_fields_page() {
         lc.myClick(lc.Setup);
         lc.myClick(lc.Parameters);
         lc.myClick(lc.Fields);
-          }
+    }
 
     @When("Create a new Field")
     public void create_a_new_field() {
         lc.myClick(lc.addBtn);
         lc.mySendKeys(lc.Name, "Nord Europa");
         lc.mySendKeys(lc.Code, "12345");
-        lc.myClick(lc.FieldTypeBtn);
-        lc.myClick(lc.LogicalBtn);
-        new Actions(WBA.getDriver()).sendKeys(Keys.ESCAPE).perform();
         lc.myClick(lc.SaveBtn);
     }
 
@@ -39,16 +32,16 @@ public class _04_AddingNewFieldsToAdminPanel extends POM_04{
 
     @Then("delete a Field")
     public void delete_a_field() {
-    lc.deleteItem("East Europa");
+        lc.deleteItem("East Europa");
 
     }
 
     @Then("User should delete successfully")
-    public void user_should_delete_successfully() throws InterruptedException{
-         lc.verifyContainsText(lc.successMessage,"deleted");
+    public void user_should_delete_successfully() throws InterruptedException {
+        lc.verifyContainsText(lc.successMessage, "deleted");
     }
 
-    }
+}
 
 
 
